9949475221f95ea56b9aff3f938fba0fb5f51f96
HakonHE18 <hakonhe@icloud.com>
2023-03-30 16:57:42 +0200

    notebook for test.py

--- src/test.py
+++ src/test.py
@@ -2,12 +2,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 import os
 from skimage.color import rgb2gray
+import PIL
 
+#DOESN'T WORK
 os.chdir("..")
 file_im = "data/images/imgs_part_1/PAT_8_15_820.png"
 im = plt.imread(file_im)
-mask = plt.imread(file_im)
-plt.imshow(mask,cmap="gray")
-mask = rgb2gray(mask)*256
+mask = plt.imshow(im,cmap="gray")
 im[mask==0]=0
-#plt.imsave("test.png",im)
+plt.imsave("test.png",im)
